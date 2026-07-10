221df464ef22c4791735ff42ed724e0fdc972221
RegiMaria <regimaria015@gmail.com>
2026-07-10 09:59:17 -0300

    feat(lsp): adiciona estrutura inicial do exemplo do carro

--- src/lsp/carro.py
+++ src/lsp/carro.py
@@ -1,6 +1,20 @@
+# ===============================================
+# Começamos com a classe Carro. Sem herança ainda.
+#================================================
+
 class Carro:
     def __init__(self):
         self.velocidade = 0
 
     def acelerar(self):
         self.velocidade += 10
+
+# Instanciando
+meu_carro = Carro()
+# print(meu_carro.velocidade)
+
+meu_carro.acelerar()
+meu_carro.acelerar()
+meu_carro.acelerar()
+print(meu_carro.velocidade)
+
